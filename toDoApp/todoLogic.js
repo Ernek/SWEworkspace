1eb18e00126a12856f6e7255b4f77c0de36918cc
ernesto <ebaezcheco@gmail.com>
2024-01-12 10:29:12 -0800

    added js logic to configure event delegation, added css for strike through when task done

--- toDoApp/todoLogic.js
+++ toDoApp/todoLogic.js
@@ -1,16 +1,40 @@
-const form = document.querySelector("#todoList");
+const form = document.querySelector("#todoFrame");
 const input = document.querySelector("#todoElement");
 const todoList = document.querySelector('#todo-List');
 
+todoList.addEventListener("click", function(e) {
+    if (e.target.tagName === "BUTTON") {
+
+        if (e.target.getAttribute('data-id') == 'doneBtn'){
+            // console.log("THIS IS THE DONE BUTTON")   
+            e.target.parentElement.classList.toggle('completed')
+            // console.log('Strike THROUGH')
+        }
+        else if (e.target.getAttribute('data-id') == 'removeBtn') {
+            // console.log("THIS IS THE REMOVE BTN")
+            e.target.parentElement.remove();
+        } else {
+            console.log("NOTHING")
+        }
+        
+    }
+})
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
     // console.log(input.value);
     const newToDo = document.createElement('li');
-    newToDo.innerText = input.value; 
+    const doneBtn = document.createElement('button');
+    doneBtn.setAttribute('data-id', 'doneBtn');
+    doneBtn.setAttribute('class', 'doneBtn');
+    // doneBtn.setAttribute('name', 'btn_name')
+    doneBtn.innerText = 'Done';
+    const removeBtn = document.createElement('button');
+    removeBtn.setAttribute('data-id', 'removeBtn');
+    removeBtn.innerText = 'Delete';
+    newToDo.innerText = input.value + '   '; 
     input.value = '';
+    newToDo.appendChild(doneBtn);
+    newToDo.appendChild(removeBtn);
     todoList.appendChild(newToDo);
 });
-
-
-// const todoElement = document.querySelectorAll("#")
