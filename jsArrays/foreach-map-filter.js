fa7302fc4ca508ab2af4a26d40215ade53288359
ernesto <ebaezcheco@gmail.com>
2024-02-09 10:51:14 -0800

    finish array methos 1

--- jsArrays/foreach-map-filter.js
+++ jsArrays/foreach-map-filter.js
@@ -159,7 +159,7 @@ Examples:
 
 function filterByValue(arr, key) {
     return arr.filter(function(obj){
-        return arr[key];
+        return arr[key] !== undefined;
     })
 }
 
@@ -171,7 +171,15 @@ Examples:
     find([1,2,3,4,5], 10) // undefined
 */
 
-function find(arr, searchValue) {}
+function find(arr, searchValue) {
+    let result = undefined
+    arr.forEach(function(value){
+        if (value === searchValue) {
+            result = value
+        } 
+    })
+    return result
+}
 
 /*
 Write a function called findInObj which accepts an array of objects, a key, and some value to search for and returns the first found value in the array.
@@ -180,7 +188,11 @@ Examples:
     findInObj([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner',true) // {first: 'Tim', last:"Garcia", isCatOwner: true}
 */
 
-function findInObj(arr, key, searchValue) {}
+function findInObj(arr, key, searchValue) {
+    return arr.filter(function(obj){
+        obj[key] === searchValue
+    })[0]
+}
 
 /*
 Write a function called removeVowels which accepts a string and returns a new string with all of the vowels (both uppercased and lowercased) removed. Every character in the new string should be lowercased.
@@ -191,7 +203,14 @@ Examples:
     removeVowels('ZZZZZZ') // ('zzzzzz')
 */
 
-function removeVowels(str) {}
+function removeVowels(str) {
+    let strLower = str.toLowerCase();
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    return Array.from(strLower).filter(function(character){
+        return !vowels.includes(character)
+    }).join("");
+
+}
 
 /*
 Write a function called doubleOddNumbers which accepts an array and returns a new array with all of the odd numbers doubled (HINT - you can use map and filter to double and then filter the odd numbers).
@@ -201,4 +220,10 @@ Examples:
     doubleOddNumbers([4,4,4,4,4]) // []
 */
 
-function doubleOddNumbers(arr) {}
+function doubleOddNumbers(arr) {
+    return (arr.filter(function(number){
+        return number % 2 !== 0;
+    })).map(function(oddNumber){
+        return oddNumber * 2;
+    })
+}
