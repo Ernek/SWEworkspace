f5f03f14de6b87feeb5d123ab757526cfa9e9d95
ernesto <ebaezcheco@gmail.com>
2024-04-08 21:53:46 -0700

    words.py example

---
+++ python-syntax/words.py
@@ -0,0 +1,6 @@
+def print_uppercase(words):
+    for word in words:
+        #print(f"{word[0].upper()}{word[1:]}")
+        print(word.upper())
+
+print_uppercase(['hours', 'house', 'yes', 'hello'])
